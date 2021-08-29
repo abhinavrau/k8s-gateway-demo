c5ad5031838f8c53f3a71e23bc71292f8a0d1c01
abhinavrau <abhinav.rau@gmail.com>
2021-08-29 21:45:39 +0000

    Try to get project id from metadata server

--- apps/foo-app/src/main/java/com/google/sp1/demo/ColorController.java
+++ apps/foo-app/src/main/java/com/google/sp1/demo/ColorController.java
@@ -1,14 +1,25 @@
 package com.google.sp1.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
-
-
+import org.springframework.web.reactive.function.client.WebClient;
 @RestController
 public class ColorController {
+    
+    HashMap<String, String> map = new HashMap<>();
+    private final WebClient webClient;
+
+    public ColorController(WebClient.Builder webClientBuilder)
+    {
+        this.webClient = webClientBuilder.baseUrl("http://metadata.google.internal/computeMetadata/v1/")
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add("Metadata-Flavor" , "Google");
+                })
+                .build();
+    }
 
     @GetMapping("/color")
     public String color() {
@@ -16,8 +27,16 @@ public class ColorController {
     }
 
     @GetMapping("/metadata")
-    public String metadata() {
-        return System.getenv().get("POD_IP");
+    public Map<String, String> metadata() {
+        
+        if (map.isEmpty()) {
+
+            map.put("POD_IP", System.getenv().get("POD_IP"));
+            map.put("POD_NAMESPACE", System.getenv().get("POD_NAMESPACE"));
+            String project = webClient.get().uri("project/project-id").retrieve().bodyToMono(String.class).block();
+            map.put("PROJECT_ID", project);
+        }
+        return map;
     }
 
 }
