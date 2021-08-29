517a76d79cec1b67dbe5cb0ba2fde29a08e9887f
msathe-tech <msathe.tech@gmail.com>
2021-08-29 10:28:06 +0000

    Change color, clean up old files

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export _SERVICES=$(kubectl get service -n foo --sort-by=.metadata.creationTimestamp | awk '{print $1}' | tail -n +2)
-export _NO_OF_SERVICES=$(echo ${_SERVICES} | wc -l)
-if [[ "${_NO_OF_SERVICES}" == "2" ]]; 
-then echo "got 2"; 
-else echo "Check your services"
-fi 
