38d604c10c267c3426b1d45c4140807186bad8a4
Madhav Sathe <32221454+msathe-tech@users.noreply.github.com>
2022-08-08 19:17:24 -0400

    Update ColorController.java

--- apps/foo-app/src/main/java/com/google/sp1/demo/ColorController.java
+++ apps/foo-app/src/main/java/com/google/sp1/demo/ColorController.java
@@ -26,7 +26,7 @@ public class ColorController {
         
         if (map.isEmpty()) {
             
-            map.put("color", "coral");
+            map.put("color", "beige");
             String cluster_name = webClient.get().uri("instance/attributes/cluster-name").retrieve().bodyToMono(String.class)
                     .block();
             map.put("ClusterName", cluster_name);
